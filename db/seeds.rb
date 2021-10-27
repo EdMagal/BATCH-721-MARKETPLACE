e8f3f3ec291f5564e41c9cdbfeafb9e3f524055f
eduardo pimenta <ed83isme@gmail.com>
2021-10-27 16:22:29 -0300

    Created a seed to populate the User and Product databases

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing users database..."
+User.destroy_all
+puts "Products database has been successfully cleared."
+
+puts "Clearning products database..."
+Product.destroy_all
+puts "Products database has been successfully cleared."
+
+# Seeds the database with 10 Instances of the User model using the Faker gem
+puts "Seeding Users database..."
+10.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_address
+  )
+end
+puts "10 users have been created."
+
+# Seeds the database with 30 Instances of the Product model using the Faker gem
+puts "Seeding products database..."
+30.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    price: rand(1..100),
+    description: Faker::Lorem.paragraph,
+    stock: rand(1..100),
+    category: Faker::Commerce.department,
+    user_id: User.all.sample.id
+  )
+end
+puts "30 products have been successfully created."
