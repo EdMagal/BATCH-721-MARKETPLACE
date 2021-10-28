8d353d1bc0785277f8c5f6890fea0af4af8c9933
Leonardo Batista <leopbatista@yahoo.com.br>
2021-10-28 15:35:03 -0300

    deals

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,32 +1,32 @@
 class DealsController < ApplicationController
-  # before_action :find_product [:new, :create]
+  before_action :find_product, only: [:new, :create]
   
   def new
     @deal = Deal.new
   end
   
   def create
-    @product = Product.find(params[:product_id])
+    # @product = Product.find(params[:product_id])
     @deal = Deal.new(deals_params)
     @deal.product = @product
-    if @deal.valid?
-      @deal.save
-      redirect_to root
+    @deal.user = current_user
+    if @deal.save
+      redirect_to products_path
     else
       render :new
     end
   end
 
-  def show
-  end
+  # def show
+  # end
 
-  def destroy  
-  end
+  # def destroy  
+  # end
 
   private
 
   def deals_params
-    params.require(:deals).permit(:quantity, :finalizado)
+    params.require(:deal).permit(:quantity)
   end
 
   def find_product
